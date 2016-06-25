26c36e5778966e874de82a89894307e5a4d1081d
gracekc <gracekc@users.noreply.github.com>
2016-06-25 11:13:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 //MODEL
 //first an array of the various locations for my markers ADD IDS
-var Attractions = [
+var Attractions = ko.observableArray([
     {name: "The Eiffel Tower", position: {lat: 48.8582, lng: 2.2945}, desc: "The Eiffel Tower was built as an entrance for the 1889 World Fair.", id: "51a2445e5019c80b56934c75"},
     {name: "The Louvre", position: {lat: 48.8611, lng: 2.3364}, desc: "The Mona Lisa is housed at the Louvre", id: "4adcda10f964a520af3521e3"},
     {name: "The Arc de Triomphe", position: {lat: 48.873756, lng: 2.294946}, desc: "The Arc de Triomphe was built in the early 19th century", id: "4adcda09f964a520de3321e3"},
     {name: "The Notre Dame", position: {lat: 48.8530, lng: 2.3498}, desc: "Notre Dame, or 'Our Lady of Paris', is a Catholic cathedral", id: "4adcda09f964a520e83321e3"},
     {name: "The Pantheon", position: {lat: 48.8461, lng: 2.3458}, desc: "The Pantheon was originally built in dedication to St. Genevieve", id: "4adcda09f964a520ea3321e3"}
-    ];
+    ]);
 
-/*function infoContent(attraction) {
-    return ('<div id="content">'+ 
-    '<div id="sideNotice">' + 
-    '</div>' +
-    '<h2 id="firstHeading" class="firstHeading"> + attractions</h2>' +
-    '<div id="bodyContent">' +
-    '<p>The Arc de Triomphe was built in the early 19th century</p>' +
-    '</div>'+
-    '</div>';)
-} */
+
 var infoWindow;
 
 //rendering the map and giving it coordinates
@@ -34,6 +25,7 @@ function ViewModel() {
 
   var self = this;
   self.markers = [];
+  self.Attractions = ko.observableArray(Attractions);
 
   self.Attractions().forEach(function(attraction) {
     var marker = new google.maps.Marker({
@@ -55,14 +47,16 @@ function ViewModel() {
           async: true
       });
           var infoWindow = new google.maps.InfoWindow({
-            content: '<div id="content">'+ 
+            content: attraction.desc
+
+            /*'<div id="content">'+ 
             '<div id="sideNotice">' + 
             '</div>' +
             '<h2 id="firstHeading" class="firstHeading">attraction.name</h2>' +
             '<div id="bodyContent">' +
             '<p>attraction.desc</p>' +
           '</div>'+
-          '</div>'
+          '</div>'*/
           });
 
           attraction.infoWindow = infoWindow;
